bafe96f740fb0b5d245cd612ae9c395def523890
Oscar Zuñiga Lara <30669700+oscarzunigalara@users.noreply.github.com>
2020-02-22 14:48:52 -0600

    A1

---
+++ assigment1/cmake-build-debug/CMakeFiles/assigment1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assigment1.dir/main.c.o"
+  "assigment1"
+  "assigment1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assigment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
