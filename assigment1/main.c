31d98297e0fae66d41ad04bba2a89fe24a71231d
Oscar Zuñiga Lara <30669700+oscarzunigalara@users.noreply.github.com>
2020-02-22 16:56:02 -0600

    a2

--- assigment1/main.c
+++ assigment1/main.c
@@ -1,11 +1,83 @@
 #include <stdio.h>
 
+
+/*
+ * A. DIFFERENCE
+
 int main() {
     printf("Hello, World!\n");
     return 0;
-
-
 }
+*/
 
+/*
+ * A. printlSlashes
 
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
 
+/*
+ * A. Stewie
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
+
+/*
+ * A. Spikey
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
+
+/*
+ * A. Stewie 2
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
+
+/*
+ * A. Much Better
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
+
+/*
+ * A. Egg
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
+
+/*
+ * A. Meta
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
+
+/*
+ * A. Mantra
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
---
+++ assigment2/cmake-build-debug/CMakeFiles/assigment2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assigment2.dir/main.c.o"
+  "assigment2"
+  "assigment2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assigment2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
