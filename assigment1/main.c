de14e9db84e65b99a1cb4179d24d2f211284c38d
Oscar Zuñiga Lara <30669700+oscarzunigalara@users.noreply.github.com>
2020-02-22 18:44:18 -0600

    A1, completada

--- assigment1/main.c
+++ assigment1/main.c
@@ -3,27 +3,32 @@
 
 /*
  * A. DIFFERENCE
-
 int main() {
-    printf("Hello, World!\n");
+    printf("What is the difference between a ' and a \"? Or between a \" and a \\\"?\n"
+           "\n"
+           "One is what we see when we're typing our program. \n The other is what appears on the \"console.\"\n");
     return 0;
 }
 */
+
 
 /*
  * A. printlSlashes
 
 int main() {
-    printf("Hello, World!\n");
+    printf("/ \\ // \\\\ /// \\\\\\\n");
     return 0;
 }
 */
+
 
 /*
  * A. Stewie
 
 int main() {
-    printf("Hello, World!\n");
+    printf("//////////////////////\n"
+           "|| Victory is mine! ||\n"
+           "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
     return 0;
 }
 */
@@ -32,16 +37,27 @@ int main() {
  * A. Spikey
 
 int main() {
-    printf("Hello, World!\n");
+    printf("   \\/\n"
+           "  \\\\//\n"
+           " \\\\\\///\n"
+           " ///\\\\\\\n"
+           "  //\\\\\n"
+           "   /\\\n"
+           "\n");
     return 0;
 }
-*/
+ */
+
 
 /*
  * A. Stewie 2
 
 int main() {
-    printf("Hello, World!\n");
+    printf("////////////////////// \n");
+    for (int i = 0; i<5; i++){
+        printf("|| Victory is mine! ||\n"
+               "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ \n");
+    }
     return 0;
 }
 */
@@ -50,7 +66,12 @@ int main() {
  * A. Much Better
 
 int main() {
-    printf("Hello, World!\n");
+    printf("A \"quoted\" String is\n"
+           "'much' better if you learn\n"
+           "the rules of \"escape sequences.\"\n"
+           "Also, \"\" represents an empty String.\n"
+           "Don't forget: use \\\" instead of \" !\n"
+           "'' is not the same as \" \n");
     return 0;
 }
 */
@@ -59,7 +80,12 @@ int main() {
  * A. Egg
 
 int main() {
-    printf("Hello, World!\n");
+    printf("  _______\n"
+           " /       \\\n"
+           "/         \\\n"
+           "-\"-'-\"-'-\"-\n"
+           "\\         /\n"
+           " \\_______/\n");
     return 0;
 }
 */
@@ -68,16 +94,27 @@ int main() {
  * A. Meta
 
 int main() {
-    printf("Hello, World!\n");
+    printf("#include \"stdio.h\"\n"
+           "\n"
+           "\n"
+           "int main(void) {\n"
+           "printf(\"Hello World\\n\");\n"
+           "return 0;\n"
+           "}\n");
     return 0;
 }
 */
+
 
 /*
  * A. Mantra
-
+*/
 int main() {
-    printf("Hello, World!\n");
+
+    for (int i = 0; i<2; i++){
+        printf("There's one thing every coder must understand:\n"
+               "The printf command. \n");
+    }
+
     return 0;
 }
-*/
--- assigment2/main.c
+++ assigment2/main.c
@@ -1,6 +1,109 @@
 #include <stdio.h>
 
+/*
+ * A. CompleteLoop
+
 int main() {
     printf("Hello, World!\n");
     return 0;
 }
+*/
+
+/*
+ * A. Count
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
+
+/*
+ * A. ThreeDigitsFunction
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
+
+/*
+ * A. startSquare
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
+
+/*
+ * A. WaveNumer40
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
+
+/*
+ * A. NestedNUmbers
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
+
+/*
+ * A. PrintDesign
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
+
+/*
+ * A. SlashFigure
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
+
+/*
+ * A. SlashFigure2
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
+
+/*
+ * A. DollarFigure
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
+
+/*
+ * A. ToManyRockets
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
+
+/*
+ * A. NumbersOutput
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+*/
