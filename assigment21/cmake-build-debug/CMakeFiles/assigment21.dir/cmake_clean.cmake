586e2b1f78b25fe3791587779ea700660a79e57c
tndrdesk <A01654827@itesm.mx>
2020-02-24 23:03:41 -0600

    a21

---
+++ assigment21/cmake-build-debug/CMakeFiles/assigment21.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assigment21.dir/main.c.o"
+  "assigment21"
+  "assigment21.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assigment21.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
