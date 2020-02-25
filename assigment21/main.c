46e2944f68259ac350987d454af0dceeeda7ad5d
tndrdesk <A01654827@itesm.mx>
2020-02-24 23:04:12 -0600

    a21

--- assigment21/main.c
+++ assigment21/main.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/*
+ * Create a program that read two parameters (row_count and col_count) from the console and prints a rectangle of "*" using one loop (The amount of rows of the rectangle is row_count and the amount of columns is col_count).
+This assignment is optional and must be solved without using arrays.
+This assignment must be solved individually.
+Submission after the deadline is not valid.
+ *
+ */
+
 
 int main() {
     printf("Hello, World!\n");
