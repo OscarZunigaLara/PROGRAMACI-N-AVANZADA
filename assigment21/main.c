76f0ff7966ef8d64a9f9c06edea1c6141986ad83
Oscar Zuñiga Lara <30669700+oscarzunigalara@users.noreply.github.com>
2020-02-26 00:31:25 -0600

    a21

--- assigment21/main.c
+++ assigment21/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /*
  * Create a program that read two parameters (row_count and col_count) from the console and prints a rectangle of "*" using one loop (The amount of rows of the rectangle is row_count and the amount of columns is col_count).
 This assignment is optional and must be solved without using arrays.
@@ -8,7 +9,44 @@ Submission after the deadline is not valid.
  */
 
 
+int readRow(){
+
+    puts("INTRDODUCE row_count\n");
+    int row_count;
+    scanf("%d", &row_count);
+    return (row_count);
+}
+
+int readCol(){
+    puts("INTRODUCE col_count \n");
+    int col_count;
+    scanf("%d", &col_count);
+    return (col_count);
+}
+
 int main() {
-    printf("Hello, World!\n");
+
+    int row_count;
+
+    int col_count;
+
+    /*
+    row_count = readRow();
+    col_count = readCol();
+    */
+    row_count = 30;
+    col_count = 20;
+
+    printf("ROW COUNT %d \nCOL COUNT %d", row_count, col_count);
+    puts("................................");
+
+    for (int i = 0; i<= row_count; i++){
+        char asteriscos = '*';
+
+        printf("%0*i", col_count, 0);
+        printf("\n");
+    }
     return 0;
 }
+
+
