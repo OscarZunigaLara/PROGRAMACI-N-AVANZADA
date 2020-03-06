fc3e5035d1b5f30e256292a34a1320b5cde90762
Oscar Zuñiga Lara <30669700+oscarzunigalara@users.noreply.github.com>
2020-03-05 20:50:21 -0600

    a3

---
+++ A3/cmake-build-debug/CMakeFiles/A3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "A3"
+  "A3.pdb"
+  "CMakeFiles/A3.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/A3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
