5fef647953d375b5069ab19643e5679df387fa82
tndrpoplap <A01654827@itesm.mx>
2020-03-08 15:38:07 -0600

    a3

--- A3/main.c
+++ A3/main.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+/*A
+void printer(double x, double y, double bubble) {
+    printf("x = %.2f and y = %.1f\n", x, y);
+    printf("x = %.2f and y = %.4f\n", x, bubble);
+    printf("The value from main is: %.4f\n", bubble);
+}
+
+int main(void) {
+    double bubble = 867.5309;
+    double x = 10.01;
+    double y = 8.0;
+    int z = 5;
+
+    printer( x,  y, bubble);
+
+    printf("z = %d", z);
+
+    return 0;
+}
+ */
+
+/*
+ B
+
+
+
+ */
+
+void printStrings(char *i, int n){
+    for (int j = 0; j < n; ++j) {
+        while(*i - '\0')
+        {
+            if(*i==' ') {*i = '_';}
+            i++;
+        }
+    }
+
+}
+int main (void){
+    printStrings("abc", 5);
     return 0;
 }
