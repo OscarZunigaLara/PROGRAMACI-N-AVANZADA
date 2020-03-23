a1147d8174c67a169bea9255f18a8e47d2fdba33
tndrdesk <a01654827@itesm.mx>
2020-03-22 18:13:00 -0600

    ex

---
+++ EXAMEN1/cmake-build-debug/CMakeFiles/EXAMEN1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EXAMEN1.dir/main.c.o"
+  "EXAMEN1"
+  "EXAMEN1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EXAMEN1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ EXAMEN1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int codificaBits(){
+
+
+
+
+
+}
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
