17f297f5c4ed4f516d837ba5ea0cd2fa27066950
tndrdesk <a01654827@itesm.mx>
2020-04-24 14:06:49 -0500

    a7

---
+++ A7/cmake-build-debug/CMakeFiles/A7.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "A7"
+  "A7.pdb"
+  "CMakeFiles/A7.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/A7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ A7/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+int prtSimplifyFraction(){
+
+}
+int ptrSumFraction(){
+
+}
+
+int ptrEuclideanDistance3D(){
+
+}
+
+int ptrQuadrant(){
+
+}
+
+int ptrComputeSlope(){
+
+}
+
+int ptrAreCollinear(){
+
+}
+
+int ptrRectangleContainsPoint(){
+
+}
+int ptrRectangleUnion(){
+
+}
+
+int ptrRectangleIntersection(){
+
+}
+
+int ptrPointInsideConvexPolygon(){
+
+}
+
+
+int main() {
+    return 0;
+}
