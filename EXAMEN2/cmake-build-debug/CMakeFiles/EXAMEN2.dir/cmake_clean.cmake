e3256e5fa540961277d17eb00942f318f5d50938
tndrmanjvbox <a01654827@itesm.mx>
2020-05-01 14:25:47 -0500

    a7

---
+++ EXAMEN2/cmake-build-debug/CMakeFiles/EXAMEN2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EXAMEN2.dir/main.c.o"
+  "EXAMEN2"
+  "EXAMEN2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EXAMEN2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ EXAMEN2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+typedef struct {
+    unsigned int x;
+    unsigned int y;
+    float angle;
+    enum MinutiaType = {"Ending", "Bifurcation","Unknown"};
+
+
+}Minutia;
+
+
+int main() {
+
+    return 0;
+}
