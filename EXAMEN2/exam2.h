28a3213c4207487a0ca4abce371ad7d71d907798
tndrmanjvbox <a01654827@itesm.mx>
2020-05-07 20:06:02 -0500

    EX

--- EXAMEN2/exam2.h
+++ EXAMEN2/exam2.h
@@ -28,16 +28,19 @@ typedef struct{
 }MinutiaArray;
 
 
-
+///GENERATES A MINUTIA, RETURNS THE POINTER
 Minutia * createMinutia(unsigned short x, unsigned short y, float angle, MinutiaType type);
 
+///GENERATES AN ARRAY OF SIZE lenght AND VALUES INITIALIZED ON 0, RETURNS THE POINTER
 MinutiaArray * createMinutiaArray(unsigned short length);
+///RELEASES DE ARRAY,
+void releaseMinutiaArray(MinutiaArray *arrMin);
 
-int releaseMinutiaArray(MinutiaArray *arrMin);
-
+///COMPUTE THE DISTANCE BETWEEN TWO MINUTIAS
 double computeDistance(const Minutia *const minutia, const Minutia *const minutia1);
 
-Minutia *findCentroid(const MinutiaArray * const minutiaArr, double (*computeDis)(const Minutia *const minutia, const Minutia *const minutia1));
+////FINDS THE CENTROID ON THE ARRAY, RETURNS THE POINTER
+Minutia *findCentroid(const MinutiaArray * const minutiaArr, double (*computeDistance)(const Minutia *const minutia, const Minutia *const minutia1));
 
 
 
--- EXAMEN2/main.c
+++ EXAMEN2/main.c
@@ -46,10 +46,9 @@ MinutiaArray * createMinutiaArray(unsigned short length){
     return arrMin;
 }
 
-int releaseMinutiaArray(MinutiaArray *arrMin){
+void releaseMinutiaArray(MinutiaArray *arrMin){
     free(arrMin->minutiae);
     free(arrMin);
-    return 0;
 }
 
 double computeDistance(const Minutia *const minutia, const Minutia *const minutia1){
@@ -62,17 +61,17 @@ double computeDistance(const Minutia *const minutia, const Minutia *const minuti
     return distance;
 }
 
-Minutia *findCentroid(const MinutiaArray * const minutiaArr, double (*computeDis)(const Minutia *const minutia, const Minutia *const minutia1)){
+Minutia *findCentroid(const MinutiaArray * const minutiaArr, double (*computeDistance)(const Minutia *const minutia, const Minutia *const minutia1)){
     double distancia = 0;
     double min = 0x7FEFFFFFFFFFFFFFl;
     int pos = 0;
 
-    for (int i = 0; i < minutiaArr->length; ++i) {
+    for (int i = 0; i < minutiaArr->length; i++) {
 
         distancia = 0;
-        for (int j = 0; j < minutiaArr->length; ++j) {
+        for (int j = 0; j < minutiaArr->length; j++) {
 
-            distancia =distancia + computeDis(&minutiaArr->minutiae[i],&minutiaArr->minutiae[j]);
+            distancia =distancia + computeDistance(&minutiaArr->minutiae[i],&minutiaArr->minutiae[j]);
         }
 
         if (distancia < min){
@@ -85,10 +84,140 @@ Minutia *findCentroid(const MinutiaArray * const minutiaArr, double (*computeDis
     return &minutiaArr->minutiae[pos];
 }
 
+
+
 int testFindCentroid(){
     int found;
-    int siezeOfArr = 20;
-    MinutiaArray *minutiaArr = createMinutiaArray(siezeOfArr);
+    int siezeOfArr = 10;
+    MinutiaArray *MinutiaArray = createMinutiaArray(siezeOfArr);
+
+
+    Minutia *min1 = createMinutia(1, 11, 12 , Unknown);
+    MinutiaArray->minutiae[0] = *min1;
+    free(min1);
+
+    min1 = createMinutia(2, 12, 54, Unknown);
+    MinutiaArray->minutiae[1] = *min1;
+    free(min1);
+
+    min1 = createMinutia(3, 13, 55, Unknown);
+    MinutiaArray->minutiae[2] = *min1;
+    free(min1);
+
+    min1 = createMinutia(4, 14, 12 , Unknown);
+    MinutiaArray->minutiae[3] = *min1;
+    free(min1);
+
+    min1= createMinutia(5, 15, 1 , Unknown);
+    MinutiaArray->minutiae[4] = *min1;
+    free(min1);
+
+    min1= createMinutia(6, 16, 12 , Unknown);
+    MinutiaArray->minutiae[5] = *min1;
+    free(min1);
+
+    min1= createMinutia(7, 17, 12 , Bifurcation);
+    MinutiaArray->minutiae[6] = *min1;
+    free(min1);
+
+    min1 = createMinutia(8, 18, 12 , Unknown);
+    MinutiaArray->minutiae[7] = *min1;
+    free(min1);
+
+    min1 = createMinutia(9, 19,12 , Bifurcation);
+    MinutiaArray->minutiae[8] = *min1;
+    free(min1);
+
+    min1 = createMinutia(10, 20, 12 , Unknown);
+    MinutiaArray->minutiae[9] = *min1;
+    free(min1);
+
+
+    Minutia *centroid = findCentroid(MinutiaArray,computeDistance);
+
+    //printf(" \n %d %d \n", centroid->x, centroid->y);
+    //printf(" \n %d %d \n", (&MinutiaArray->minutiae[siezeOfArr/2-1])->x, (&MinutiaArray->minutiae[siezeOfArr/2-1])->y);
+
+    if (&MinutiaArray->minutiae[siezeOfArr/2 -1 ] == centroid){
+        found = 1;
+    }else
+        found = 0;
+    releaseMinutiaArray(MinutiaArray);
+
+
+    return found;
+
+}
+
+int testFindCentroid2(){
+    int found;
+    int siezeOfArr = 10;
+    MinutiaArray *MinutiaArray = createMinutiaArray(siezeOfArr);
+
+
+    Minutia *min1 = createMinutia(1, 11, 12 , Unknown);
+    MinutiaArray->minutiae[0] = *min1;
+    free(min1);
+
+    Minutia *min2 = createMinutia(2, 12, 54, Ending);
+    MinutiaArray->minutiae[1] = *min2;
+    free(min2);
+
+    Minutia *min3 = createMinutia(4, 13, 55, Unknown);
+    MinutiaArray->minutiae[2] = *min3;
+    free(min3);
+
+    Minutia *min4 = createMinutia(8, 14, 12 , Unknown);
+    MinutiaArray->minutiae[3] = *min4;
+    free(min4);
+
+    Minutia *min5 = createMinutia(16, 15, 1 , Ending);
+    MinutiaArray->minutiae[4] = *min5;
+    free(min5);
+
+    Minutia *min6 = createMinutia(32, 16, 12 , Unknown);
+    MinutiaArray->minutiae[5] = *min6;
+    free(min6);
+
+    Minutia *min7 = createMinutia(64, 17, 12 , Bifurcation);
+    MinutiaArray->minutiae[6] = *min7;
+    free(min7);
+
+    Minutia *min8 = createMinutia(128, 18, 12 , Unknown);
+    MinutiaArray->minutiae[7] = *min8;
+    free(min8);
+
+    Minutia *min9 = createMinutia(256, 19,12 , Bifurcation);
+    MinutiaArray->minutiae[8] = *min9;
+    free(min9);
+
+    Minutia *min10 = createMinutia(512, 20, 12 , Unknown);
+    MinutiaArray->minutiae[9] = *min10;
+    free(min10);
+
+
+    Minutia *centroid = findCentroid(MinutiaArray,computeDistance);
+
+    //printf(" \n %d %d \n", centroid->x, centroid->y);
+    //printf(" \n %d %d \n", (&MinutiaArray->minutiae[siezeOfArr/2-1])->x, (&MinutiaArray->minutiae[siezeOfArr/2-1])->y);
+
+    if (&MinutiaArray->minutiae[siezeOfArr/2 -1 ] == findCentroid(MinutiaArray,computeDistance)){
+        found = 1;
+    }else
+        found = 0;
+    releaseMinutiaArray(MinutiaArray);
+
+
+    return found;
+
+}
+
+
+
+int testFindCentroidMiPrueba(){
+    int found;
+    int siezeOfArr = 10;
+    MinutiaArray *MinutiaArray = createMinutiaArray(siezeOfArr);
 
     for (int i = 0; i < siezeOfArr; ++i) {
 
@@ -96,81 +225,49 @@ int testFindCentroid(){
         //printf("%d",type);
         if(type == 0) {
             //printf("0");
-            Minutia *minutia = createMinutia(i * 2, i * 3, i * 30 , Unknown);
-            minutiaArr->minutiae[i] = *minutia;
-            free(minutia);
+            Minutia *min = createMinutia(i , i +10, i * 30 , Unknown);
+            MinutiaArray->minutiae[i] = *min;
+            free(min);
         }
         if (type == 1){
             //printf("1");
-            Minutia *minutia = createMinutia(i * 2, i * 3, i * 30 , Bifurcation);
-            minutiaArr->minutiae[i] = *minutia;
-            free(minutia);
+            Minutia *min = createMinutia(i , i +10, i * 30 , Bifurcation);
+            MinutiaArray->minutiae[i] = *min;
+            free(min);
         }
         if (type == 2){
             //printf("2");
-            Minutia *minutia = createMinutia(i * 2, i * 3, i * 30 , Ending);
-            minutiaArr->minutiae[i] = *minutia;
-            free(minutia);
+            Minutia *min = createMinutia(i, i +10, i * 30 , Ending);
+            MinutiaArray->minutiae[i] = *min;
+            free(min);
         }
 
 
     }
-    Minutia *centroid = findCentroid(minutiaArr,computeDistance);
-    printf(" \n %d %d \n", centroid->x, centroid->y);
-    printf(" \n %d %d \n", centroid->x, centroid->y);
-    if (&minutiaArr->minutiae[siezeOfArr/2] == findCentroid(minutiaArr,computeDistance)){
+
+
+
+    Minutia *centroid = findCentroid(MinutiaArray,computeDistance);
+
+    //printf(" \n %d %d \n", centroid->x, centroid->y);
+    //printf(" \n %d %d \n", (&MinutiaArray->minutiae[siezeOfArr/2 -1])->x, (&MinutiaArray->minutiae[siezeOfArr/2 -1 ])->y);
+
+
+    if (&MinutiaArray->minutiae[siezeOfArr/2 -1] == centroid){
         found = 1;
     }else
         found = 0;
-    releaseMinutiaArray(minutiaArr);
-
+    releaseMinutiaArray(MinutiaArray);
 
     return found;
 
 
 }
 
-
-int testFindCentroidMiPrueba(){
-    int found;
-    int siezeOfArr = 10;
-    MinutiaArray *minutiaArr = createMinutiaArray(siezeOfArr);
-
-
-    Minutia *minutia = createMinutia(3, 4, 12 , Unknown);
-    minutiaArr->minutiae[0] = *minutia;
-    free(minutia);
-
-    Minutia *minutia1 = createMinutia(5, 3, 54, Ending);
-    minutiaArr->minutiae[1] = *minutia;
-    free(minutia);
-
-    Minutia *minutia2 = createMinutia(55, 1, 55, Unknown);
-    minutiaArr->minutiae[2] = *minutia;
-    free(minutia);
-
-    Minutia *minutia3 = createMinutia(3, 4, 12 , Unknown);
-    minutiaArr->minutiae[0] = *minutia;
-    free(minutia);
-
-
-
-
-
-
-    if (&minutiaArr->minutiae[siezeOfArr/2] == findCentroid(minutiaArr,computeDistance)){
-        found = 1;
-    }else
-        found = 0;
-    releaseMinutiaArray(minutiaArr);
-
-
-}
-
 int testFindCentroidMiPrueba2(){
         int found;
-        int siezeOfArr = 10;
-        MinutiaArray *minutiaArr = createMinutiaArray(siezeOfArr);
+        int siezeOfArr = 100;
+        MinutiaArray *MinutiaArray = createMinutiaArray(siezeOfArr);
 
         for (int i = 0; i < siezeOfArr; ++i) {
 
@@ -178,38 +275,37 @@ int testFindCentroidMiPrueba2(){
             //printf("%d",type);
             if(type == 0) {
                 //printf("0");
-                Minutia *minutia = createMinutia(i * 2 * (type + 1), i * 3 * (type + 1), i * 30 , Unknown);
-                minutiaArr->minutiae[i] = *minutia;
-                free(minutia);
+                Minutia *min = createMinutia(i * 2 * (type + 1), i * 3 * (type + 1), i * 30 , Unknown);
+                MinutiaArray->minutiae[i] = *min;
+                free(min);
             }
             if (type == 1){
                 //printf("1");
-                Minutia *minutia = createMinutia(i * 2 * (type + 1), i * 3 * (type + 1), i * 30 , Bifurcation);
-                minutiaArr->minutiae[i] = *minutia;
-                free(minutia);
+                Minutia *min = createMinutia(i * 2 * (type + 1), i * 3 * (type + 1), i * 30 , Bifurcation);
+                MinutiaArray->minutiae[i] = *min;
+                free(min);
             }
             if (type == 2){
                 //printf("2");
-                Minutia *minutia = createMinutia(i * 2 * (type + 1), i * 3 * (type + 1), i * 30 , Ending);
-                minutiaArr->minutiae[i] = *minutia;
-                free(minutia);
+                Minutia *min = createMinutia(i * 2 * (type + 1), i * 3 * (type + 1), i * 30 , Ending);
+                MinutiaArray->minutiae[i] = *min;
+                free(min);
             }
 
 
         }
-        Minutia *centroid = findCentroid(minutiaArr,computeDistance);
-        printf(" \n %d %d \n", centroid->x, centroid->y);
-        printf(" \n %d %d \n", (&minutiaArr->minutiae[siezeOfArr/2])->x, (&minutiaArr->minutiae[siezeOfArr/2])->y);
+    Minutia *centroid = findCentroid(MinutiaArray,computeDistance);
 
-    if (&minutiaArr->minutiae[siezeOfArr/2] == findCentroid(minutiaArr,computeDistance)){
-            found = 1;
-        }else
-            found = 0;
-        releaseMinutiaArray(minutiaArr);
+    //printf(" \n %d %d \n", centroid->x, centroid->y);
+    //printf(" \n %d %d \n", (&MinutiaArray->minutiae[siezeOfArr/2 -1])->x, (&MinutiaArray->minutiae[siezeOfArr/2 -1])->y);
 
+    if (&MinutiaArray->minutiae[siezeOfArr/2 - 1] == findCentroid(MinutiaArray,computeDistance)){
+        found = 1;
+    }else
+        found = 0;
+    releaseMinutiaArray(MinutiaArray);
 
         return found;
-
 
 }
 
@@ -220,9 +316,10 @@ int main() {
     printf("EXAMEN 2");
     int exito;
 
-    //exito = testFindCentroid();
-
-    exito = testFindCentroidMiPrueba2();
+    exito = testFindCentroid();
+    //exito = testFindCentroid2();
+    //exito = testFindCentroidMiPrueba();
+    //exito = testFindCentroidMiPrueba2();
     printf("\n %d",exito);
     return 0;
 }
