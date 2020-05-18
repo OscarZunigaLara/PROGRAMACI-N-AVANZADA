8dea4cb39b432705f40ab33941d07fe78df20052
tndrmanjvbox <a01654827@itesm.mx>
2020-05-18 00:40:52 -0500

    a8

--- A8/main.c
+++ A8/main.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+typedef enum{
+    numerical,
+    nominal
+}FeatureType;
+
+
+typedef struct {
+    double * features;
+    FeatureType * featureTypes;
+    int length;
+}Instance;
+
+
+
+Instance * generateInstance(const int length){
+    Instance * instanciaPTR = (Instance *) malloc(sizeof(Instance));
+    instanciaPTR->length = length;
+    instanciaPTR->features = malloc(length * sizeof(double));
+    instanciaPTR->featureTypes = malloc(length * sizeof(FeatureType));
+
+    return instanciaPTR;
+}
+
+void liberaInstance(Instance * instance){
+    free(instance->featureTypes);
+    free(instance->features);
+    free(instance);
+}
+
+
+
+Instance * findNearestNeighbor(const Instance * const instances, const int length, const Instance * const query,
+                               double (*dissimilarityFunction)(const Instance * const instanceOne, const Instance * const instanceTwo)) {
+
+
+}
+
+
+double computeEuclideanDissimilarity(const Instance * const instanceOnePtr, const Instance * const instanceTwoPtr){
+    double dob;
+    return dob;
+}
+
 
 int main() {
-    printf("Hello, World!\n");
+    printf("TAREA 8\n");
+
+
+
     return 0;
 }
