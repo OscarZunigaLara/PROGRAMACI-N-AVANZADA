cd3a221bb3730f162883e729a67b4155e8b855e5
tndrdesk <A01654827@itesm.mx>
2020-06-04 15:43:19 -0500

    a8

--- A8/cmake-build-debug/CMakeFiles/A8.dir/DependInfo.cmake
+++ A8/cmake-build-debug/CMakeFiles/A8.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/tndrmanjvbox/CLionProjects/PROGRAMACI-N-AVANZADA/A8/main.c" "/home/tndrmanjvbox/CLionProjects/PROGRAMACI-N-AVANZADA/A8/cmake-build-debug/CMakeFiles/A8.dir/main.c.o"
+  "/home/tndrdesk/CLionProjects/PROGRAMACI-N-AVANZADA/A8/main.c" "/home/tndrdesk/CLionProjects/PROGRAMACI-N-AVANZADA/A8/cmake-build-debug/CMakeFiles/A8.dir/main.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- A8/main.c
+++ A8/main.c
@@ -46,11 +46,12 @@ double computeEuclideanDissimilarity(const Instance * const firstInstancePointer
         if (firstInstancePointer->featureTypes[i] != secondInstancePointer->featureTypes[i]){
             return -3;
         }
-
-        if (firstInstancePointer->featureTypes[i] == 0){
-            double firstVal = firstInstancePointer->features[i];
-            double secondVal = secondInstancePointer->features[i];
-            sum += firstInstancePointer->features[i] != secondInstancePointer->features[i] ? 1: 0;
+        if (firstInstancePointer->featureTypes[i] == 0 && secondInstancePointer->featureTypes[i] == 0) {
+            double value1 = firstInstancePointer->features[i];
+            double value2 = firstInstancePointer->features[i];
+            sum += pow(value1 - value2, 2);
+        } else if (firstInstancePointer->featureTypes[i] == 1 && secondInstancePointer->featureTypes[i] == 1){
+            sum += firstInstancePointer->features[i] != secondInstancePointer->features[i] ? 1 : 0;
         }
 
     }
@@ -62,18 +63,76 @@ double computeEuclideanDissimilarity(const Instance * const firstInstancePointer
 
 
 Instance * findNearestNeighbor(const Instance * const instances, const int length, const Instance * const query,
-                               double (*dissimilarityFunction)(const Instance * const instanceOne, const Instance * const instanceTwo)) {
+                               double (*function)(const Instance * const firstInstancePointer, const Instance * const secondInstancePointer)) {
 
     const int dist;
+    double menor = 0;
+    menor = function(&instances[0], query);
+    int ind = 0;
 
+    if (menor == -1 || menor == -2 || menor == -3){
+        return (int)menor;
+    }
+    double nuevoMenor = 0;
+
+
+    for (int j = 0; j < dist-1; j++) {
+        nuevoMenor +=function(&instances[j], query);
+        if (menor == -1 || menor == -2 || menor == -3){
+            return (int)menor;
+        }
+        if (nuevoMenor< menor){
+        menor =nuevoMenor;
+        }
+
+    }
+    return &instances[ind];
+
+}
+
+int testfindNearestNeighbor0(){
+
+}
+
+int testfindNearestNeighborMenos1(){
+    Instance  * firstInstance = generateInstance(3);
+
+    firstInstance->features[0] = 0;
+    firstInstance->features[1] = 0;
+    firstInstance->features[2] = 0;
+
+    firstInstance->featureTypes[0] = 0;
+    firstInstance->featureTypes[0] = 0;
+    firstInstance->featureTypes[0] = 0;
+
+    Instance * secondInstance = generateInstance(0);
+    double  disSim = computeEuclideanDissimilarity(firstInstance,secondInstance);
+
+    liberaInstancia(firstInstance);
+    liberaInstancia(secondInstance);
+
+    return disSim;
 
 
 }
 
+int testfindNearestNeighbor2(){
+
+}
+
+int testfindNearestNeighbor3(){
+
+}
+
+int testfindNearestNeighbor(){
+
+}
 
 
 int main() {
     printf("TAREA 8\n");
+
+    printf("PRUEBA DE MENOS  1 %d",testfindNearestNeighborMenos1() );
 
 
 
